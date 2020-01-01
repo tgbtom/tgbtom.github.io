b85e091d8153c525bfddd6ddde2de005f52410f0
Thomas Baldwin <tgbtom_20@Hotmail.com>
2020-01-01 15:44:51 -0500

    Revert to Defaul index

--- resources/views/lineitem/create.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <form action="/line" method="post">
-        <input type="submit" value="Create Recipe Line">
-        {{csrf_field()}}
-    </form>    
-@endsection
